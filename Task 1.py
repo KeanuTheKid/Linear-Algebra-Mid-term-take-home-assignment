194c3dc78f3117ea3be210ed9454d0e46a1ba1b1
Keanu <121106291+KeanuTheKid@users.noreply.github.com>
2024-11-04 17:33:35 +0200

    Task 1 done

---
+++ Task 1.py
@@ -0,0 +1,81 @@
+import time
+import random
+import numpy as np
+import csv
+#matrices are none other than lists of lists
+matrix_A = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+#keeping in mind that python starts to count at 0
+matrix_B = [
+    [4,5,3],
+    [3,4,8],
+    [1,2,1]
+]
+for rows_a in matrix_A:#iterating through rows
+    #print(rows_a)
+    for element_a in rows_a:#iterating through elements
+        #print(element_a)
+        pass
+ #matrix multiplication is row * column so I need a way to iterate through columns
+# this function basically iterates through the rows 
+# and takes the "Index" element of a row and puts its in a new list, which gives us a column
+def matrix_column(Matrix, Index):
+    column = []
+    for row in Matrix:
+        column.append(row[Index])
+    return column
+#as we can can get rows easily with a for loop, there is no reason to make a function
+
+#now that we can iterate through rows and columns we can multiply   
+def matrix_multiplication(A, B):
+    result = [] #result matrix
+    for row in A: #iterate through rows
+        row_result = [] 
+        for column_index in range(len(B[0])): #iterate through columns
+            column = matrix_column(B, column_index)
+            product_sum = 0
+            for i in range(len(row)): #iterates through indices of the elements
+                product_sum += row[i] * column[i]
+            row_result.append(product_sum) # appends the sum of the products 
+        result.append(row_result) # appends result rows to the matrix
+    return result
+
+def gen_matrices(size):
+    matrix = []
+    for k in range(size):#for columns
+        row = []
+        for i in range(size):
+            row.append(random.randint(-99,99)) #generates rows that are "size" long
+        matrix.append(row)#appends there rows to the matrix
+    return matrix
+
+
+
+with open('matrix_times.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    # header
+    writer.writerow(['Matrixgröße', 'Laufzeit Eigene Implementierung (s)', 'Laufzeit NumPy (s)'])
+    
+    
+    for i in range(2, 200):
+        A = gen_matrices(i)
+        B = gen_matrices(i)
+
+        start_time = time.time()
+        multiplication = matrix_multiplication(A, B)
+        end_time = time.time()
+        computing_time = (end_time - start_time)
+
+        np_A = np.array(A)
+        np_B = np.array(B)
+        start_time = time.time()
+        np_multiplication = np.matmul(np_A, np_B)
+        end_time = time.time()
+        np_computing_time = (end_time - start_time)
+
+        print(f"size: {i} own function: {computing_time}s , numpy: {np_computing_time}s")
+        writer.writerow([i, computing_time, np_computing_time])
+
